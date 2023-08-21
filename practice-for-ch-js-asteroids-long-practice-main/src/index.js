b1bc18043c2986540f50f47b90ad6109f77d7585
Sean <pca.sean.jeun@gmail.com>
2023-08-21 16:21:08 -0400

    circle drawn

--- practice-for-ch-js-asteroids-long-practice-main/src/index.js
+++ practice-for-ch-js-asteroids-long-practice-main/src/index.js
@@ -3,3 +3,14 @@ console.log("Webpack is packin!")
 import MovingObject from "./moving-object.js";
 
 window.MovingObject = MovingObject;
+const moving = new MovingObject({
+    pos: [120, 120],
+    vel: [10, 10],
+    radius: 100,
+    color: "#FF0000"
+})
+
+const canvasEle = document.getElementById("game-canvas")
+const ctx = canvasEle.getContext("2d")
+
+moving.draw(ctx)
--- practice-for-ch-js-asteroids-long-practice-main/src/moving-object.js
+++ practice-for-ch-js-asteroids-long-practice-main/src/moving-object.js
@@ -1,9 +1,19 @@
 class MovingObject {
     constructor(option) {
-        this.pos = option[pos];
-        this.vel = option[vel];
-        this.radius = option[radius];
-        this.color = option[color];
+        this.pos = option.pos;
+        this.vel = option.vel;
+        this.radius = option.radius;
+        this.color = option.color;
+    }
+
+    draw(ctx) {
+        const x = this.pos[0];
+        const y = this.pos[1];
+        ctx.beginPath();
+        ctx.arc(x, y, this.radius, 0, 2 * Math.PI, true);
+        ctx.fillStyle = `${this.color}`
+        ctx.stroke();
+        ctx.fill()
     }
 }
 
